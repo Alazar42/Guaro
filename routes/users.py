946c510e771006f709dd1f41dbafc862bbe29723
Alazar42 <alazartesfaye42@gmail.com>
2026-06-02 12:37:30 +0300

    Disable authentication requirement for user retrieval endpoints

--- routes/users.py
+++ routes/users.py
@@ -7,12 +7,12 @@ router = Router(prefix="/users")
 
 
 @router.get("/")
-@require_auth
+# @require_auth
 def get_users() -> list[User]:
     return User.all()
 
 
 @router.get("/{id}")
-@require_auth
+# @require_auth
 def get_user(id: int) -> User | None:
     return User.find(id)
